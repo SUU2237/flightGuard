aae082032ceea1c2e785c1267e19bf07ba15867b
SUU2237 <siayu1030@gmail.com>
2026-08-12 16:37:46 +0800

    fix: 修正 GitHub Pages 部署靜態資源路徑

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/flightGuard/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
