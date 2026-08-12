dc14ad102d67dc885e85114ef02f6756dac93637
SUU2237 <siayu1030@gmail.com>
2026-08-12 17:06:06 +0800

    fix: 解決 SPA 路由 404 問題

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 // src/router/index.ts
 
-import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router';
+import { createRouter, type RouteRecordRaw ,createWebHashHistory } from 'vue-router';
 
 /**
  * 路由設定清單
@@ -38,7 +38,7 @@ const routes: RouteRecordRaw[] = [
  * 使用 HTML5 History 模式 (createWebHistory)
  */
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
